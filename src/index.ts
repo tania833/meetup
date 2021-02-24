160cdbbdd33365f125b693cf383722a7e83818a6
tania833 <tania.polischuk1998@gmail.com>
2021-02-24 14:41:03 +0200

    add vuetify

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import app from './App.vue';
 import './index.scss';
-import { createStore } from './store/index'
+import { createStore } from './store/index';
+import vuetify from './plugins/vuetify';
 
 const App = new Vue({
     el: '#root',
     store: createStore(),
+    vuetify,
     render: (h) => h(app),
 });
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,13 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export const createStore = () => new Vuex.Store({
-    state: {
-        count: 0,
-    },
-    mutations: {
-        increment: (state) => state.count++,
-        decrement: (state) => state.count--,
-    },
-});
+export const createStore = () =>
+    new Vuex.Store({
+        state: {
+            count: 0,
+        },
+        mutations: {
+            increment: (state) => state.count++,
+            decrement: (state) => state.count--,
+        },
+    });
